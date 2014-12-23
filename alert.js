4798d7bed9479dc602ac995e987d93b7e774f919
Dharani Manne <dharani.manne@gmail.com>
2014-12-23 18:13:37 +0530

    version 0.0.2 console logging

--- alert.js
+++ alert.js
@@ -3,5 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
   var button = document.getElementById("oyy3");
   button.addEventListener('click', function() {
     console.log("the button is clicked.");
+    var username = document.getElementById("oyy1").value;
+    var password = document.getElementById("oyy2").value;
+    alert("The username is :" + username + " and the password is : " + password + ".");
   });
 });
